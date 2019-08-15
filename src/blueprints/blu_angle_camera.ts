947dd3fa9dbcde2868755b3d937bd0ca30960262
Staś Małolepszy <stas@duzodobrze.pl>
2019-08-15 18:02:29 +0200

    Tilt the camera 30° on X

--- src/blueprints/blu_angle_camera.ts
+++ src/blueprints/blu_angle_camera.ts
@@ -5,7 +5,8 @@ export let angle_camera_blueprint: Blueprint = {
     children: [
         {
             translation: [5, 5, 5],
-            rotation: [-0.354, 0.354, 0.146, 0.854],
+            // Y -45°, X -30°, Z 0°
+            rotation: [-0.239, 0.37, 0.099, 0.892],
             using: [camera_ortho(7, 0.1, 1000)],
         },
     ],
