8dcbc74001a5794d51eb1af57c4f00e5410857a3
Staś Małolepszy <stas@duzodobrze.pl>
2019-08-16 14:22:29 +0200

    Make the cubes selectable to avoid creating new ones too close to the existing ones

--- src/systems/sys_navigate.ts
+++ src/systems/sys_navigate.ts
@@ -2,6 +2,7 @@ import {collide} from "../components/com_collide.js";
 import {Get} from "../components/com_index.js";
 import {render_shaded} from "../components/com_render_shaded.js";
 import {rigid_body} from "../components/com_rigid_body.js";
+import {selectable} from "../components/com_selectable.js";
 import {Game} from "../game.js";
 import {Mat} from "../materials/mat_index.js";
 import {Cube} from "../shapes/Cube.js";
@@ -12,14 +13,15 @@ const QUERY =
 export function sys_navigate(game: Game, delta: number) {
     for (let i = game.world.length; i >= 0; i--) {
         if ((game.world[i] & QUERY) === QUERY) {
-            let selectable = game[Get.Selectable][i];
-            if (selectable.selected && game.event.mouse_0_down) {
+            let selection = game[Get.Selectable][i];
+            if (selection.selected && game.event.mouse_0_down) {
                 game.add({
-                    translation: selectable.hit,
+                    translation: selection.hit,
                     using: [
                         render_shaded(game.materials[Mat.Flat], Cube, [0.3, 1, 1, 1]),
                         collide(false),
                         rigid_body(false),
+                        selectable(),
                     ],
                 });
             }
