fe85dc2866a10d4ad184465d779e781bfa334824
Staś Małolepszy <stas@duzodobrze.pl>
2019-08-20 14:31:27 +0200

    Use a true isometric projection

--- src/blueprints/blu_angle_camera.ts
+++ src/blueprints/blu_angle_camera.ts
@@ -5,9 +5,9 @@ export let angle_camera_blueprint: Blueprint = {
     children: [
         {
             translation: [5, 5, 5],
-            // Y -45°, X -30°, Z 0°
-            rotation: [-0.239, 0.37, 0.099, 0.892],
-            using: [camera_ortho(4, 1, 100)],
+            // Isometric projection: Y 45°, X -35.264°, Z 0°
+            rotation: [-0.28, 0.364, 0.116, 0.88],
+            using: [camera_ortho(5, 1, 100)],
         },
     ],
 };
