f583aa196426836ba2750a1c41da02589f759cff
Staś Małolepszy <stas@duzodobrze.pl>
2019-08-21 16:10:20 +0200

    Print more perf data

--- src/game.ts
+++ src/game.ts
@@ -32,6 +32,7 @@ import {sys_framerate} from "./systems/sys_framerate.js";
 import {sys_light} from "./systems/sys_light.js";
 import {sys_move} from "./systems/sys_move.js";
 import {sys_navigate} from "./systems/sys_navigate.js";
+import {sys_performance} from "./systems/sys_performance.js";
 import {sys_physics} from "./systems/sys_physics.js";
 import {sys_player_fly} from "./systems/sys_player_fly.js";
 import {sys_player_move} from "./systems/sys_player_move.js";
@@ -155,6 +156,8 @@ export class Game implements ComponentData {
     }
 
     fixed_update(delta: number) {
+        let now = performance.now();
+
         // Player input.
         sys_player_move(this, delta);
         sys_player_fly(this, delta);
@@ -169,6 +172,10 @@ export class Game implements ComponentData {
         sys_collide(this, delta);
         sys_physics(this, delta);
         sys_transform(this, delta);
+
+        // Performance.
+        sys_performance(this, performance.now() - now, document.querySelector("#fixed"));
+
         // Debug.
         false && sys_debug(this, delta);
 
@@ -178,12 +185,17 @@ export class Game implements ComponentData {
     }
 
     frame_update(delta: number) {
+        let now = performance.now();
+
         sys_audio(this, delta);
         sys_camera(this, delta);
         sys_light(this, delta);
         sys_render(this, delta);
-        sys_framerate(this, delta);
         sys_ui(this, delta);
+
+        // Performance.
+        sys_performance(this, performance.now() - now, document.querySelector("#frame"));
+        sys_framerate(this, delta);
     }
 
     start() {
--- src/systems/sys_framerate.ts
+++ src/systems/sys_framerate.ts
@@ -4,7 +4,6 @@ let counter = document.getElementById("fps");
 
 export function sys_framerate(game: Game, delta: number) {
     if (counter) {
-        let frame_rate = (1 / delta).toFixed(0);
-        counter.textContent = `${frame_rate} fps`;
+        counter.textContent = (1 / delta).toFixed();
     }
 }
