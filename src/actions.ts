48cf4e6b3f363ebdc1177141e6631f7bb042defa
Michał Budzyński <michal@virtualdesign.pl>
2019-08-27 23:28:36 +0200

    remove wor_stage

--- src/actions.ts
+++ src/actions.ts
@@ -2,7 +2,6 @@ import {Entity, Game} from "./game.js";
 import {world_characters} from "./worlds/wor_characters.js";
 import {world_intro} from "./worlds/wor_intro.js";
 import {world_map} from "./worlds/wor_map.js";
-import {world_stage} from "./worlds/wor_stage.js";
 
 export const enum Action {
     ChangeWorld,
@@ -15,8 +14,6 @@ export function effect(game: Game, action: Action, args: Array<unknown>) {
             switch (args[0] as string) {
                 case "intro":
                     return setTimeout(world_intro, 0, game);
-                case "stage":
-                    return setTimeout(world_stage, 0, game);
                 case "characters":
                     return setTimeout(world_characters, 0, game);
                 case "map":
