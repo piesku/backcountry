1643837e7a9267d3ef42ea540b8c72a57547cf41
Michał Budzyński <michal@virtualdesign.pl>
2019-08-27 20:12:15 +0200

    remove pitchfork

--- src/blueprints/blu_character.ts
+++ src/blueprints/blu_character.ts
@@ -1,14 +1,13 @@
-import {animate} from "../components/com_animate.js";
-import {light} from "../components/com_light.js";
-import {render_vox} from "../components/com_render_vox.js";
-import {Game} from "../game.js";
-import {from_euler} from "../math/quat.js";
-import {Models} from "../models_map.js";
-import {palette} from "../palette.js";
-import {Blueprint} from "./blu_common.js";
-import {create_gun} from "./items/blu_gun.js";
-import {create_pitchfork} from "./items/blu_pitchfork.js";
-import {create_shotgun} from "./items/blu_shotgun.js";
+import { animate } from "../components/com_animate.js";
+import { light } from "../components/com_light.js";
+import { render_vox } from "../components/com_render_vox.js";
+import { Game } from "../game.js";
+import { from_euler } from "../math/quat.js";
+import { Models } from "../models_map.js";
+import { palette } from "../palette.js";
+import { Blueprint } from "./blu_common.js";
+import { create_gun } from "./items/blu_gun.js";
+import { create_shotgun } from "./items/blu_shotgun.js";
 
 let hat_models = [
     Models.HAT1,
@@ -122,7 +121,7 @@ export function get_character_blueprint(game: Game): Blueprint {
         hair: hair_color,
     });
 
-    let items = [create_gun, create_gun, create_shotgun, create_pitchfork];
+    let items = [create_gun, create_gun, create_shotgun];
 
     let right_hand_item = Math.random() > 0.3 ? {} : items[~~(Math.random() * items.length)](game);
     let left_hand_item = Math.random() > 0.5 ? {} : items[~~(Math.random() * items.length)](game);
