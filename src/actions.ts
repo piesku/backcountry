52c9a184a9432e07ab2853173f3d9a2cef59d20d
Michał Budzyński <michal@virtualdesign.pl>
2019-08-29 03:16:42 +0200

    maze generation

--- src/actions.ts
+++ src/actions.ts
@@ -2,6 +2,7 @@ import {Get} from "./components/com_index.js";
 import {Entity, Game} from "./game.js";
 import {world_intro} from "./worlds/wor_intro.js";
 import {world_map} from "./worlds/wor_map.js";
+import {world_mine} from "./worlds/wor_mine.js";
 
 export const enum Action {
     ChangeWorld,
@@ -17,6 +18,8 @@ export function effect(game: Game, action: Action, args: Array<unknown>) {
                     return setTimeout(world_intro, 0, game);
                 case "map":
                     return setTimeout(world_map, 0, game);
+                case "mine":
+                    return setTimeout(world_mine, 0, game);
             }
         }
         case Action.HitEnemy: {
