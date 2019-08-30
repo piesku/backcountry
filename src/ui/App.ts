734b3a7b3f122660f0c6771296612b7fb5e5c121
Staś Małolepszy <stas@duzodobrze.pl>
2019-08-30 23:25:24 +0200

    Remove Overlay UI

--- src/ui/App.ts
+++ src/ui/App.ts
@@ -1,12 +1,11 @@
 import {GameState} from "../actions.js";
 import {html} from "./html.js";
 import {Intro} from "./Intro.js";
-import {Overlay} from "./Overlay.js";
 import {Wanted} from "./Wanted.js";
 
 export function App(state: GameState) {
     return html`
-        ${Overlay(state)} ${state.world === "intro" && Intro(state.seed_town)}
+        ${state.world === "intro" && Intro(state.seed_town)}
         ${state.world === "wanted" && Wanted(state.seed_house)}
     `;
 }
