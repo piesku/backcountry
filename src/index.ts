817f74bdfca95da4e0820bc95b6dfb97d908f9e0
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-01 17:07:51 +0200

    Remove window.game

--- src/index.ts
+++ src/index.ts
@@ -5,8 +5,6 @@ import {world_intro} from "./worlds/wor_intro.js";
 let game = new Game();
 // @ts-ignore
 window.$ = game.dispatch;
-// @ts-ignore
-window.game = game;
 
 async function start() {
     game.models = await load("./models.tfu");
