227be3266c8438c1f669003e3b122be1546165b9
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-01 18:06:34 +0200

    Mangle Named

--- src/components/com_named.ts
+++ src/components/com_named.ts
@@ -2,20 +2,20 @@ import {Entity, Game} from "../game.js";
 import {Get} from "./com_index.js";
 
 export interface Named {
-    name: string;
+    Name: string;
 }
 
-export function named(name: string) {
+export function named(Name: string) {
     return (game: Game) => (entity: Entity) => {
         game.world[entity] |= 1 << Get.Named;
-        game[Get.Named][entity] = <Named>{name};
+        game[Get.Named][entity] = <Named>{Name};
     };
 }
 
 export function find_first(game: Game, name: string) {
     for (let i = 0; i < game.world.length; i++) {
         if (game.world[i] & (1 << Get.Named)) {
-            if (game[Get.Named][i].name === name) {
+            if (game[Get.Named][i].Name === name) {
                 return i;
             }
         }
