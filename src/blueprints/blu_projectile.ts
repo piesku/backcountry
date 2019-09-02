9585f91bdf6a882e2dcb9503aee0678ee0122145
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-02 15:33:06 +0200

    Add particles and light to the projectile

--- src/blueprints/blu_projectile.ts
+++ src/blueprints/blu_projectile.ts
@@ -1,6 +1,9 @@
 import {collide} from "../components/com_collide.js";
+import {emit_particles} from "../components/com_emit_particles.js";
+import {light} from "../components/com_light.js";
 import {move} from "../components/com_move.js";
 import {projectile} from "../components/com_projectile.js";
+import {render_particles} from "../components/com_render_particles.js";
 import {render_vox} from "../components/com_render_vox.js";
 import {Blueprint} from "./blu_common";
 
@@ -11,6 +14,12 @@ export function create_projectile() {
             projectile(1, 5),
             move(40),
             render_vox({Offsets: new Float32Array(4), Size: [1, 1, 1]}, [1, 0, 0]),
+            light([1, 1, 0], 2),
+        ],
+        Children: [
+            {
+                Using: [emit_particles(1, 0.07, 3, 0, 5), render_particles([1, 0, 0], [1, 1, 0])],
+            },
         ],
     };
 }
