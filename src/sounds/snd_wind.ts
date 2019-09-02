c32e367f7e514a45110e90c7f477368f1e743adf
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-02 17:08:19 +0200

    Add wind sound clips

---
+++ src/sounds/snd_wind.ts
@@ -0,0 +1,11 @@
+import {AudioClip} from "../components/com_audio_source";
+
+export let snd_wind = <AudioClip>{
+    Tracks: [
+        {
+            Instrument: [7, true, "lowpass", 8, 6, true, true, "sine", 8, 2, [[1, 3, 4, 2, 10]]],
+            Notes: [57],
+        },
+    ],
+    Exit: 13,
+};
