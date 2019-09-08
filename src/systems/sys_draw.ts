a6c5c0ad6857c60e954adadee1af1901ea74d8e0
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-08 21:53:25 +0200

    (sys_draw) Inline update

--- src/systems/sys_draw.ts
+++ src/systems/sys_draw.ts
@@ -1,5 +1,5 @@
 import {Get} from "../components/com_index.js";
-import {Entity, Game} from "../game.js";
+import {Game} from "../game.js";
 import {Vec3} from "../math/index.js";
 import {get_translation} from "../math/mat4.js";
 import {transform_point} from "../math/vec3.js";
@@ -8,25 +8,21 @@ const QUERY = (1 << Get.Transform) | (1 << Get.Draw);
 
 export function sys_draw(game: Game, delta: number) {
     game.Context.clearRect(0, 0, game.Canvas2.width, game.Canvas2.height);
+    let position = [] as Vec3;
 
     for (let i = 0; i < game.World.length; i++) {
         if ((game.World[i] & QUERY) === QUERY) {
-            update(game, i);
+            // World position.
+            get_translation(position, game[Get.Transform][i].World);
+            // NDC position.
+            transform_point(position, position, game.Camera!.PV);
+
+            game[Get.Draw][i].Widget(
+                game,
+                i,
+                0.5 * (position[0] + 1) * game.Canvas3.width,
+                0.5 * (-position[1] + 1) * game.Canvas3.height
+            );
         }
     }
-}
-
-function update(game: Game, entity: Entity) {
-    let position = [] as Vec3;
-    // World position.
-    get_translation(position, game[Get.Transform][entity].World);
-    // NDC position.
-    transform_point(position, position, game.Camera!.PV);
-
-    game[Get.Draw][entity].Widget(
-        game,
-        entity,
-        0.5 * (position[0] + 1) * game.Canvas3.width,
-        0.5 * (-position[1] + 1) * game.Canvas3.height
-    );
 }
