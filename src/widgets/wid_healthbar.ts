8830d8db911d5e4929a49a4b81de7baeae9f49a9
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-08 20:50:44 +0200

    (widget_healthbar) Adjust the width of the health bar

--- src/widgets/wid_healthbar.ts
+++ src/widgets/wid_healthbar.ts
@@ -1,6 +1,15 @@
+import {Get} from "../components/com_index";
 import {Entity, Game} from "../game";
 
 export function widget_healthbar(game: Game, entity: Entity, x: number, y: number) {
+    // Health bars must be direct children of character containers.
+    let parent = game[Get.Transform][entity].Parent!.Entity;
+    let health = game[Get.Health][parent];
     game.Context.fillStyle = "#0f0";
-    game.Context.fillRect(x - 100, y, 200, 10);
+    game.Context.fillRect(
+        x - 0.05 * game.Canvas2.width,
+        y,
+        (0.1 * game.Canvas2.width * health.Current) / health.Max,
+        0.01 * game.Canvas2.height
+    );
 }
