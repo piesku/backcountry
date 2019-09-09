c0a16da98f473ae8fc813be7271dfd0ff016ebc0
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-09 22:36:39 +0200

    (sys_cull) Use DOMMatrix and DOMPoint directly

--- src/systems/sys_cull.ts
+++ src/systems/sys_cull.ts
@@ -1,7 +1,5 @@
 import {Get} from "../components/com_index.js";
 import {Entity, Game} from "../game.js";
-import {get_translation} from "../math/mat4.js";
-import {transform_point} from "../math/vec3.js";
 
 const QUERY = (1 << Get.Transform) | (1 << Get.Cull);
 
@@ -17,15 +15,16 @@ export function sys_cull(game: Game, delta: number) {
 
 function update(game: Game, entity: Entity) {
     let cull = game[Get.Cull][entity];
-    let world_position = get_translation([], game[Get.Transform][entity].World);
-    let camera_position = transform_point([], world_position, game.Camera!.View);
+    let world = game[Get.Transform][entity].World;
+    let world_position = new DOMPoint(world.m41, world.m42, world.m43);
+    let camera_position = game.Camera!.View.transformPoint(world_position);
     if (
         // m11 of the ortho projection matrix is defined as 1/right. Cull
         // transforms to the left and to the right of the frustum, with a padding.
-        Math.abs(camera_position[0]) > 1 / game.Camera!.Projection.m11 + 8 ||
+        Math.abs(camera_position.x) > 1 / game.Camera!.Projection.m11 + 8 ||
         // m22 of the ortho projection matrix is defined as 1/top. Cull
         // transforms above and below the frustum, with a padding.
-        Math.abs(camera_position[1]) > 1 / game.Camera!.Projection.m22 + 8
+        Math.abs(camera_position.y) > 1 / game.Camera!.Projection.m22 + 8
     ) {
         game.World[entity] &= ~(1 << cull.Component);
     } else {
