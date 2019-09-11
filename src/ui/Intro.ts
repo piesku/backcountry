aec46dc41f12bf21d07410610e0cb6731d398cc2
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-11 19:27:45 +0200

    New Intro CTA

--- src/ui/Intro.ts
+++ src/ui/Intro.ts
@@ -3,15 +3,21 @@ import {Action} from "../actions.js";
 export function Intro() {
     return `
         <div style="
-            left: 50%;
+            left: 5%;
             display: flex;
+            width: 50%;
             height: 100%;
         ">
-            <div style="margin: auto 0; font-size: 15vmin;">
-                BACK<br/>COUNTRY
-                <hr style="border: solid #D45230 10px;" />
-                <div onclick="$(${Action.GoToTown});">
-                    PLAY
+            <div style="margin: auto 0;">
+                BACK<br>COUNTRY
+                <div onclick="$(${Action.GoToTown});" style="
+                    border-top: 20px solid #d45230;
+                ">
+                    Play Now
+                </div>
+                <div>
+                    Earn as much money as you can in today's challenge.
+                    Check <a href="https://twitter.com">#backcountryrpg</a> for high scores.
                 </div>
             </div>
         </div>
