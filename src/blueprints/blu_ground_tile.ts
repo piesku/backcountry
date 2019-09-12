3162d5f64f2dd37a0bbe5b2bcf68196cc6b2dda0
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-12 17:05:24 +0200

    (blu_ground_tile) Merge two if blocks

--- src/blueprints/blu_ground_tile.ts
+++ src/blueprints/blu_ground_tile.ts
@@ -62,12 +62,6 @@ export function get_tile_blueprint(
         Children: [],
     };
 
-    if (rand() > 0.85 && is_walkable) {
-        tile.Children!.push(get_block_blueprint(game));
-    } else if (has_gold && rand() < 0.01 && is_walkable) {
-        tile.Children!.push(get_gold_blueprint(game));
-    }
-
     if (!is_walkable) {
         tile.Children!.push(
             rand() > 0.5
@@ -76,6 +70,10 @@ export function get_tile_blueprint(
                 ? get_rock_blueprint(game)
                 : get_campfire_blueprint(game)
         );
+    } else if (rand() > 0.85) {
+        tile.Children!.push(get_block_blueprint(game));
+    } else if (has_gold && rand() < 0.01) {
+        tile.Children!.push(get_gold_blueprint(game));
     }
 
     return {
