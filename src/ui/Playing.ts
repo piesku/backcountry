c6fcb89db9aa7421fbee3a3393b7b2585ad0b64b
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-12 11:08:32 +0200

    Use margin for positioning score

--- src/ui/Playing.ts
+++ src/ui/Playing.ts
@@ -3,9 +3,7 @@ import {GameState} from "../actions.js";
 export function Playing(state: GameState) {
     return `
         <div style="
-            position: absolute;
-            left: 5vmin;
-            top: 4vmin;
+            margin: 4% 5%;
             font: 10vmin Impact;
         ">
             $${state.Gold.toLocaleString("en-US")}
