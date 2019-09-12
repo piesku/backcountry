d3315c9bd2228d7cbcb03d0662f7076847a66390
Michał Budzyński <michal@virtualdesign.pl>
2019-09-12 11:08:52 +0200

    just one cactus

--- src/blueprints/blu_cactus.ts
+++ src/blueprints/blu_cactus.ts
@@ -2,18 +2,14 @@ import {cull} from "../components/com_cull.js";
 import {Get} from "../components/com_index.js";
 import {render_vox} from "../components/com_render_vox.js";
 import {Game} from "../game.js";
-import {element} from "../math/random.js";
+import {float} from "../math/random.js";
 import {Models} from "../models_map.js";
 import {Blueprint} from "./blu_common.js";
 
-let cacti_indexes_and_heights = [[Models.CAC1VOX, 4], [Models.CAC2, 3], [Models.CAC3, 4.5]];
-
 export function get_cactus_blueprint(game: Game): Blueprint {
-    let cacti = element(cacti_indexes_and_heights);
-
-    let model = game.Models[(cacti as any)[0] as number];
+    let model = game.Models[Models.CAC3];
     return {
-        Translation: [0, (cacti as any)[1], 0],
+        Translation: [0, float(2, 4.5), 0],
         Using: [render_vox(model), cull(Get.Render)],
     };
 }
