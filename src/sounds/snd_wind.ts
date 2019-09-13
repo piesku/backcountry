a105d16c814619489a1e1dc281e88043e593be44
Staś Małolepszy <stas@duzodobrze.pl>
2019-09-13 09:46:25 +0200

    Longer wind

--- src/sounds/snd_wind.ts
+++ src/sounds/snd_wind.ts
@@ -3,7 +3,7 @@ import {AudioClip} from "../components/com_audio_source";
 export let snd_wind = <AudioClip>{
     Tracks: [
         {
-            Instrument: [7, "lowpass", 8, 6, true, "sine", 8, 2, [[false, 3, 4, 2, 10]]],
+            Instrument: [7, "lowpass", 8, 6, true, "sine", 9, 2, [[false, 3, 6, 4, 13]]],
             Notes: [57],
         },
     ],
